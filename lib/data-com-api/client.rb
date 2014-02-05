a652002eaf829112f4b28c8582e9aea6d679729b
Fire-Dragon-DoL <francesco.belladonna@gmail.com>
2014-02-05 22:48:40 +0100

    Wrote some tests

--- lib/data-com-api/client.rb
+++ lib/data-com-api/client.rb
@@ -8,9 +8,25 @@ module DataComApi
     TIME_ZONE      = 'Pacific Time (US & Canada)'.freeze
 
     def initialize(api_token=nil)
-      @token = api_token || ENV[ENV_NAME_TOKEN]
+      @token     = api_token || ENV[ENV_NAME_TOKEN]
+      @page_size = 50
 
       raise TokenFailError, 'No token set!' unless @token
+    end
+
+    def page_size
+      @page_size
+    end
+
+    # Page size = 0 returns objects count only (small request)
+    def page_size=(value)
+      real_value = value.to_i
+
+      if real_value < 0 || real_value > 100
+        raise ParamError, "page_size must be between 0 and 100, received #{ real_value }"
+      end
+
+      @page_size = real_value
     end
 
     private
--- spec/requests/client_spec.rb
+++ spec/requests/client_spec.rb
@@ -6,7 +6,7 @@ describe DataComApi::Client do
   subject(:client) { FactoryGirl.build(:client) }
 
   it "has a valid factory" do
-    expect{subject}.not_to raise_error
+    expect{client}.not_to raise_error
   end
 
   context "env #{ described_class::ENV_NAME_TOKEN } empty" do
@@ -25,7 +25,21 @@ describe DataComApi::Client do
 
   end
 
-  
+  it "#page_size= accepts values between 0 and 100" do
+    expect{client.page_size = Random.rand(101)}.not_to raise_error
+  end
+
+  it "#page_size= doesn't accept values > 100" do
+    expect{client.page_size = 101}.to raise_error DataComApi::ParamError
+  end
+
+  it "#page_size= doesn't accept values < 0" do
+    expect{client.page_size = (-1)}.to raise_error DataComApi::ParamError
+  end
+
+  it "#search_contact without params returns size > 0" do
+    expect{client.search_contact.size}.to > 0
+  end
 
   # it "#company_contact_count" do
   #   contacts = client.company_contact_count('existing_company_id')
