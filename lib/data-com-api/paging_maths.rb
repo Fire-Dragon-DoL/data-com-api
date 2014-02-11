03e008443e582e8add356994923a1e50ba51a461
Fire-Dragon-DoL <francesco.belladonna@gmail.com>
2014-02-11 01:47:20 +0100

    Insolvable problem with offset 100 000

--- lib/data-com-api/paging_maths.rb
+++ lib/data-com-api/paging_maths.rb
@@ -1,10 +1,12 @@
+require 'active_support/cache'
+
 module DataComApi
 
   class PagingMaths
 
-    attr_accessor :page_size
-    attr_accessor :max_offset
-    attr_accessor :total_records
+    attr_reader :page_size
+    attr_reader :max_offset
+    attr_reader :total_records
 
     def initialize(options={})
       options = {
@@ -13,14 +15,122 @@ module DataComApi
         max_offset:    1_000_000  
       }.merge(options)
 
-      self.page_size     = options[:page_size]
-      self.max_offset    = options[:max_offset]
-      self.total_records = options[:total_records]
+      @cache         = ActiveSupport::Cache::MemoryStore.new
+      @page_size     = options[:page_size]
+      @max_offset    = options[:max_offset]
+      @total_records = options[:total_records]
     end
 
-    def any_record?
-      self.page_size > 0 && self.max_offset > 0 && self.total_records > 0
+    # Fields
+
+    def page_size=(value)
+      @page_size = value
+      cache.clear
     end
+
+    def max_offset=(value)
+      @max_offset = value
+      cache.clear
+    end
+
+    def total_records=(value)
+      @total_records = value
+      cache.clear
+    end
+
+    # Methods
+
+    def any_record?
+      cache.fetch(:any_record?) do
+        self.page_size > 0 && self.max_offset > 0 && self.total_records > 0
+      end
+    end
+
+    def total_pages
+      cache.fetch(:total_pages) do
+        next 0 unless any_record?
+
+        records_amount = self.total_records
+        records_amount = self.max_offset if self.total_records > self.max_offset
+        res            = records_amount / self.page_size
+        res           += 1 unless (records_amount % self.page_size) == 0
+
+        res
+      end
+    end
+
+    def page_index(page)
+      return nil unless self.any_record?
+
+      page = case page
+      when :first then 1
+      when :last  then self.total_pages
+      else page.to_i
+      end
+
+      cache_page = :"page_index_#{ page }"
+      return cache.read(cache_page) if cache.exist? cache_page
+
+      case page
+      when 0   then raise ArgumentError, "Page index can't be 0"
+      when nil then raise ArgumentError, "Page index can't be nil"
+      end
+
+      page = nil if page > self.total_pages
+
+      cache.write(cache_page, page)
+      page
+    end
+
+    def page_from_offset(value)
+      return nil unless self.any_record?
+
+      cache_page = :"page_from_offset_#{ value }"
+      return cache.read(cache_page) if cache.exist? cache_page
+
+      if value > self.max_offset
+        raise ArgumentError, <<-eos
+          Offset must not be greater than max_offset (##{ self.max_offset })
+        eos
+      end
+
+      page  = value / self.page_size
+      page += 1 unless (value % self.page_size) == 0
+      page  = 1 if value < self.page_size
+
+      cache.write(cache_page, page)
+      page
+    end
+
+    def offset_from_page(value)
+      return nil unless self.any_record?
+
+      cache_page = :"offset_from_page_#{ value }"
+      return cache.read(cache_page) if cache.exist? cache_page
+
+      if value > self.max_offset
+        raise ArgumentError, "Offset can't be greater than max_offset"
+      end
+      page = self.page_index(value)
+
+      binding.pry
+      # This happens when page > total_pages
+      return nil if page.nil?
+
+      records_count = self.total_records
+      records_count = self.max_offset if records_count > self.max_offset
+      offset = (page - 1) * page_size
+      offset = records_count if page == self.total_pages
+
+      cache.write(cache_page, offset)
+      offset
+    end
+
+    private
+
+      def cache
+        @cache
+      end
 
   end
 
--- spec/models/paging_maths_spec.rb
+++ spec/models/paging_maths_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 require 'data-com-api/paging_maths'
 
-describe DataComApi::PagingMaths, focus: true do
+describe DataComApi::PagingMaths do
   subject(:paging_maths) do
     FactoryGirl.build(:paging_maths,
       max_offset:    100_000,
@@ -51,18 +51,27 @@ describe DataComApi::PagingMaths, focus: true do
       expect{paging_maths.page_index(nil)}.to raise_error ArgumentError
     end
 
+    it "doesn't raise error when value > total_pages is passed" do
+      expect{paging_maths.page_index(paging_maths.total_pages + 1)}.not_to raise_error
+    end
+
+    it "is nil when value > total_pages is passed" do
+      expect(paging_maths.page_index(paging_maths.total_pages + 1)).to be_nil
+    end
+
     context "when records available" do
 
       it "is 1 when using :first" do
         expect(paging_maths.page_index(:first)).to be 1
       end
 
-      it "is 1_000 when using :last" do
+      it "is 1 000 when using :last" do
         expect(paging_maths.page_index(:last)).to be 1_000
       end
 
-      it "is 33334 when using :last and page_size is 3" do
-        expect(paging_maths.page_index(:last)).to be 33334
+      it "is 33 334 when using :last and page_size is 3" do
+        paging_maths.page_size = 3
+        expect(paging_maths.page_index(:last)).to be 33_334
       end
 
     end
@@ -73,12 +82,13 @@ describe DataComApi::PagingMaths, focus: true do
       end
 
       # Just to ensure that subject of subject doesn't create disasters
-      it "has no records available" do
+      it "ensures has no records" do
         expect(paging_maths.total_records).to be 0
       end
 
       [:first, :last, 2, 3].each do |random_page|
-        it "is nil when no records are available" do
+        it "is nil" do
+          paging_maths.total_records = 0
           expect(paging_maths.page_index(random_page)).to be_nil
         end
       end
@@ -97,9 +107,9 @@ describe DataComApi::PagingMaths, focus: true do
       expect(paging_maths.records_per_page(:first)).to be paging_maths.records_per_page(1)
     end
 
-    it "is the same amount of records when using 33334 (last) with page_size 3 or :last" do
+    it "is the same amount of records when using 33 334 (last) with page_size 3 or :last" do
       paging_maths.page_size = 3
-      expect(paging_maths.records_per_page(:last)).to be paging_maths.records_per_page(33334)
+      expect(paging_maths.records_per_page(:last)).to be paging_maths.records_per_page(33_334)
     end
 
     it "is nil when page_size = 0" do
@@ -117,9 +127,9 @@ describe DataComApi::PagingMaths, focus: true do
       expect(paging_maths.records_per_page(1)).to be_nil
     end
 
-    it "is 1 on page 33334 with page_size 3" do
+    it "is 1 on page 33 334 with page_size 3" do
       paging_maths.page_size  = 3
-      expect(paging_maths.records_per_page(33334)).to be 1
+      expect(paging_maths.records_per_page(33_334)).to be 1
     end
 
     it "is page_size when not on last page" do
@@ -130,6 +140,10 @@ describe DataComApi::PagingMaths, focus: true do
 
   describe "#page_from_offset" do
 
+    it "raises error when offset > max_offset" do
+      expect{paging_maths.page_from_offset(paging_maths.max_offset + 1)}.to raise_error ArgumentError
+    end
+
     it "is nil when no records available" do
       paging_maths.total_records = 0
       expect(paging_maths.page_from_offset(0)).to be_nil
@@ -137,6 +151,51 @@ describe DataComApi::PagingMaths, focus: true do
 
     it "is 1 when offset is 0" do
       expect(paging_maths.page_from_offset(0)).to be 1
+    end
+
+    it "is 100 when offset is 10 000" do
+      expect(paging_maths.page_from_offset(10_000)).to be 100
+    end
+
+    it "is 33 334 when offset is 100 000 and page_size = 3" do
+      paging_maths.page_size = 3
+      expect(paging_maths.page_from_offset(100_000)).to be 33_334
+    end
+
+  end
+
+  describe "#offset_from_page" do
+
+    it "doesn't raise error when page > total_pages" do
+      expect{paging_maths.offset_from_page(paging_maths.total_pages + 1)}.not_to raise_error
+    end
+
+    it "is nil when no records available" do
+      paging_maths.total_records = 0
+      expect(paging_maths.offset_from_page(1)).to be_nil
+    end
+
+    it "is nil when page > total_pages" do
+      expect(paging_maths.offset_from_page(paging_maths.total_pages + 1)).to be_nil
+    end
+
+    it "is 0 when page is 1" do
+      expect(paging_maths.offset_from_page(1)).to be 0
+    end
+
+    it "is 10_000 when page is 100", focus: true do
+      expect(paging_maths.offset_from_page(100)).to be 10_000
+    end
+
+    it "is 100 000 when page is 33 334 and page_size = 3" do
+      paging_maths.page_size = 3
+      expect(paging_maths.offset_from_page(33_334)).to be 100_000
+    end
+
+    it "is 100 000 when page is 33 334 and page_size = 3 and total_records 99 999", focus: true do
+      paging_maths.page_size     = 3
+      paging_maths.total_records = 99_999
+      expect(paging_maths.offset_from_page(33_334)).to be 100_000
     end
 
   end
@@ -152,9 +211,9 @@ describe DataComApi::PagingMaths, focus: true do
       expect(paging_maths.total_pages).to be 1_000
     end
 
-    it "is 33334 when page_size = 3" do
+    it "is 33 334 when page_size = 3" do
       paging_maths.page_size = 3
-      expect(paging_maths.total_pages).to be 33334
+      expect(paging_maths.total_pages).to be 33_334
     end
 
     it "is equal to total_records when total_records < page_size" do
